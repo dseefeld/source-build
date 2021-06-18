8dfe3ea27c1331c1a4fa9b6ac583564a8681ce4b
dseefeld <dseefeld@microsoft.com>
2021-06-18 19:58:18 +0000

    Compare tarball content for arpow tarball against release/5.0

--- build.sh
+++ build.sh
@@ -5,57 +5,43 @@ IFS=$'\n\t'
 usage() {
     echo "usage: $0 [options]"
     echo "options:"
-    echo "  -test                          run supported individal repo unit tests"
-    echo "  --run-smoke-test               run smoke tests"
-    echo "  --publish-prebuilt-report      publish prebuilt report data to Azure Storage"
-    echo "  --generate-prebuilt-data       generate prebuilt burndown data"
-    echo "  --with-sdk <dir>               use the SDK in the specified directory for bootstrapping"
-    echo "extra arguments will be passed to MSBuild"
+    echo "  --with-ref-packages <dir>          use the specified directory of reference packages"
+    echo "  --with-packages <dir>              use the specified directory of previously-built packages"
+    echo "  --with-sdk <dir>                   use the SDK in the specified directory for bootstrapping"
+    echo "use -- to send the remaining arguments to MSBuild"
     echo ""
 }
 
-# resolve $SOURCE until the file is no longer a symlink
-source="${BASH_SOURCE[0]}"
-scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
-while [[ -h $source ]]; do
-  scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
-  source="$(readlink "$source")"
+SCRIPT_ROOT="$(cd -P "$( dirname "$0" )" && pwd)"
 
-  # if $source was a relative symlink, we need to resolve it relative to the path where the
-  # symlink file was located
-  [[ $source != /* ]] && source="$scriptroot/$source"
-done
-
-if [ -z "${HOME:-}" ]; then
-    export HOME="$scriptroot/.home"
-    mkdir "$HOME"
-fi
-
-if grep -q '\(/docker/\|/docker-\)' "/proc/1/cgroup"; then
-    export DotNetRunningInDocker=1
-fi
-
+MSBUILD_ARGUMENTS=("/p:OfflineBuild=true" "/flp:v=detailed")
+CUSTOM_REF_PACKAGES_DIR=''
+CUSTOM_PREVIOUSLY_BUILT_PACKAGES_DIR=''
 alternateTarget=false
 CUSTOM_SDK_DIR=''
 
-for arg do
-  shift
-  opt="$(echo "$arg" | awk '{print tolower($0)}')"
-  case $opt in
-    (-test) set -- "$@" "/t:RunTests"
+while :; do
+    if [ $# -le 0 ]; then
+        break
+    fi
+
+    lowerI="$(echo $1 | awk '{print tolower($0)}')"
+    case $lowerI in
+        --run-smoke-test)
             alternateTarget=true
+            MSBUILD_ARGUMENTS+=( "/t:RunSmokeTest" )
             ;;
-    (--run-smoke-test) set -- "$@" "/t:RunSmokeTest"
-            alternateTarget=true
+        --with-packages)
+            CUSTOM_PREVIOUSLY_BUILT_PACKAGES_DIR="$(cd -P "$2" && pwd)"
+            if [ ! -d "$CUSTOM_PREVIOUSLY_BUILT_PACKAGES_DIR" ]; then
+                echo "Custom prviously built packages directory '$CUSTOM_PREVIOUSLY_BUILT_PACKAGES_DIR' does not exist"
+                exit 1
+            fi
+            MSBUILD_ARGUMENTS+=( "/p:CustomPrebuiltSourceBuiltPackagesPath=$CUSTOM_PREVIOUSLY_BUILT_PACKAGES_DIR" )
+            shift
             ;;
-    (--publish-prebuilt-report) set -- "$@" "/t:PublishPrebuiltReportData"
-            alternateTarget=true
-            ;;
-    (--generate-prebuilt-data) set -- "$@" "/t:GeneratePrebuiltBurndownData"
-            alternateTarget=true
-            ;;
-    (--with-sdk)
-            CUSTOM_SDK_DIR="$(cd -P "$1" && pwd)"
+        --with-sdk)
+            CUSTOM_SDK_DIR="$(cd -P "$2" && pwd)"
             if [ ! -d "$CUSTOM_SDK_DIR" ]; then
                 echo "Custom SDK directory '$CUSTOM_SDK_DIR' does not exist"
                 exit 1
@@ -66,69 +52,118 @@ for arg do
             fi
             shift
             ;;
-       (*) set -- "$@" "$arg" ;;
-  esac
+        --)
+            shift
+            echo "Detected '--': passing remaining parameters '$@' as build.sh arguments."
+            break
+            ;;
+        -?|-h|--help)
+            usage
+            exit 0
+            ;;
+        *)
+            echo "Unrecognized argument '$1'"
+            usage
+            exit 1
+            ;;
+    esac
+    shift
 done
 
-arcadeLine=`grep -m 1 'Microsoft\.DotNet\.Arcade\.Sdk' "$scriptroot/global.json"`
-sdkLine=`grep -m 1 'dotnet' "$scriptroot/global.json"`
-arcadePattern="\"Microsoft\.DotNet\.Arcade\.Sdk\" *: *\"(.*)\""
-sdkPattern="\"dotnet\" *: *\"(.*)\""
-if [[ $arcadeLine =~ $arcadePattern ]]; then
-  export ARCADE_BOOTSTRAP_VERSION=${BASH_REMATCH[1]}
-fi
-if [ -d "$CUSTOM_SDK_DIR" ]; then
-  export SDK_VERSION=`"$CUSTOM_SDK_DIR/dotnet" --version`
-  echo "Using custom bootstrap SDK from '$CUSTOM_SDK_DIR', version $SDK_VERSION"
-  CLIPATH="$CUSTOM_SDK_DIR"
-  SDKPATH="$CLIPATH/sdk/$SDK_VERSION"
-  export _InitializeDotNetCli="$CLIPATH"
-  export CustomDotNetSdkDir="$CLIPATH"
-  else
-  if [[ $sdkLine =~ $sdkPattern ]]; then
-    export SDK_VERSION=${BASH_REMATCH[1]}
-    CLIPATH="$scriptroot/.dotnet"
-    SDKPATH="$CLIPATH/sdk/$SDK_VERSION"
+if [ -f "$SCRIPT_ROOT/packages/archive/archiveArtifacts.txt" ]; then
+  ARCHIVE_ERROR=0
+  if [ ! -d "$SCRIPT_ROOT/.dotnet" ] && [ "$CUSTOM_SDK_DIR" == "" ]; then
+    echo "ERROR: SDK not found at $SCRIPT_ROOT/.dotnet"
+    ARCHIVE_ERROR=1
+  fi
+  if [ ! -f $SCRIPT_ROOT/packages/archive/Private.SourceBuilt.Artifacts*.tar.gz ] && [ "$CUSTOM_PREVIOUSLY_BUILT_PACKAGES_DIR" == "" ]; then
+    echo "ERROR: Private.SourceBuilt.Artifacts artifact not found at $SCRIPT_ROOT/packages/archive/ - Either run prep.sh or pass --with-packages parameter"
+    ARCHIVE_ERROR=1
+  fi
+  if [ $ARCHIVE_ERROR == 1 ]; then
+    echo ""
+    echo "    Errors detected in tarball. To prep the tarball, run prep.sh while online to install an SDK"
+    echo "    and Private.SourceBuilt.Artifacts tarball.  After prepping the tarball, the tarball  can be"
+    echo "    built offline.  As an alternative to prepping the tarball, these assets can be provided using"
+    echo "    the --with-sdk and --with-packages parameters"
+    exit 1
   fi
 fi
-echo "Found bootstrap SDK $SDK_VERSION, bootstrap Arcade $ARCADE_BOOTSTRAP_VERSION"
 
-if [ "$alternateTarget" == "false" ] && [[ "${SOURCE_BUILD_SKIP_SUBMODULE_CHECK:-default}" == "default" || $SOURCE_BUILD_SKIP_SUBMODULE_CHECK == "0" || $SOURCE_BUILD_SKIP_SUBMODULE_CHECK == "false" ]]; then
-  source "$scriptroot/check-submodules.sh"
+if [ -d "$CUSTOM_SDK_DIR" ]; then
+  export SDK_VERSION=`"$CUSTOM_SDK_DIR/dotnet" --version`
+  export CLI_ROOT="$CUSTOM_SDK_DIR"
+  export _InitializeDotNetCli="$CLI_ROOT/dotnet"
+  export CustomDotNetSdkDir="$CLI_ROOT"
+  echo "Using custom bootstrap SDK from '$CLI_ROOT', version '$SDK_VERSION'"
+else
+  sdkLine=`grep -m 1 'dotnet' "$SCRIPT_ROOT/global.json"`
+  sdkPattern="\"dotnet\" *: *\"(.*)\""
+  if [[ $sdkLine =~ $sdkPattern ]]; then
+    export SDK_VERSION=${BASH_REMATCH[1]}
+    export CLI_ROOT="$SCRIPT_ROOT/.dotnet"
+  fi
 fi
+
+packageVersionsPath=''
+restoredPackagesDir="$SCRIPT_ROOT/packages/restored"
+
+if [ -d "$SCRIPT_ROOT/packages/archive" ]; then
+  sourceBuiltArchive=`find $SCRIPT_ROOT/packages/archive -maxdepth 1 -name 'Private.SourceBuilt.Artifacts*.tar.gz'`
+  if [ -f "$sourceBuiltArchive" ]; then
+    tar -xzf "$sourceBuiltArchive" -C /tmp PackageVersions.props
+    packageVersionsPath=/tmp/PackageVersions.props
+  fi
+else
+  if [ -f "$CUSTOM_PREVIOUSLY_BUILT_PACKAGES_DIR/PackageVersions.props" ]; then
+    packageVersionsPath="$CUSTOM_PREVIOUSLY_BUILT_PACKAGES_DIR/PackageVersions.props"
+  fi
+fi
+
+if [ ! -f "$packageVersionsPath" ]; then
+  echo "Cannot find PackagesVersions.props.  Debugging info:"
+  echo "  Attempted archive path: $SCRIPT_ROOT/packages/archive"
+  echo "  Attempted custom PVP path: $CUSTOM_PREVIOUSLY_BUILT_PACKAGES_DIR/PackageVersions.props"
+  exit 1
+fi
+
+arcadeSdkLine=`grep -m 1 'MicrosoftDotNetArcadeSdkVersion' "$packageVersionsPath"`
+versionPattern="<MicrosoftDotNetArcadeSdkVersion>(.*)</MicrosoftDotNetArcadeSdkVersion>"
+if [[ $arcadeSdkLine =~ $versionPattern ]]; then
+  export ARCADE_BOOTSTRAP_VERSION=${BASH_REMATCH[1]}
+
+  # Ensure that by default, the bootstrap version of the Arcade SDK is used. Source-build infra
+  # projects use bootstrap Arcade SDK, and would fail to find it in the tarball build. The repo
+  # projects overwrite this so that they use the source-built Arcade SDK instad.
+  export SOURCE_BUILT_SDK_ID_ARCADE=Microsoft.DotNet.Arcade.Sdk
+  export SOURCE_BUILT_SDK_VERSION_ARCADE=$ARCADE_BOOTSTRAP_VERSION
+  export SOURCE_BUILT_SDK_DIR_ARCADE=$restoredPackagesDir/ArcadeBootstrapPackage/microsoft.dotnet.arcade.sdk/$ARCADE_BOOTSTRAP_VERSION
+fi
+
+sourceLinkLine=`grep -m 1 'MicrosoftSourceLinkCommonVersion' "$packageVersionsPath"`
+versionPattern="<MicrosoftSourceLinkCommonVersion>(.*)</MicrosoftSourceLinkCommonVersion>"
+if [[ $sourceLinkLine =~ $versionPattern ]]; then
+  export SOURCE_LINK_BOOTSTRAP_VERSION=${BASH_REMATCH[1]}
+fi
+
+# DSEEFELD TODO:  I don't think we need this anymore, but need to verify when runtime repo is included
+# dotNetHostLine=`grep -m 1 'MicrosoftNETCoreDotNetHostVersion' "$packageVersionsPath"`
+# versionPattern="<MicrosoftNETCoreDotNetHostVersion>(.*)</MicrosoftNETCoreDotNetHostVersion>"
+# if [[ $dotNetHostLine =~ $versionPattern ]]; then
+#   export DOTNET_HOST_BOOTSTRAP_VERSION=${BASH_REMATCH[1]}
+# fi
+
+echo "Found bootstrap SDK $SDK_VERSION, bootstrap Arcade $ARCADE_BOOTSTRAP_VERSION, bootstrap SourceLink $SOURCE_LINK_BOOTSTRAP_VERSION"
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-export DOTNET_MULTILEVEL_LOOKUP=0
-export NUGET_PACKAGES="$scriptroot/packages/restored/"
-
-if [ "${internalPackageFeedPat:-}" ]; then
-  echo "Setting up NuGet credential provider using PAT from env var 'internalPackageFeedPat'..."
-  . "$scriptroot/eng/install-nuget-credprovider.sh"
-  # TODO: Read these from nuget.config
-  # The internal transport isn't added by Darc, though, so it will still need special-casing.
-  export VSS_NUGET_EXTERNAL_FEED_ENDPOINTS='{"endpointCredentials": [
-    {"endpoint":"https://pkgs.dev.azure.com/dnceng/public/_packaging/darc-pub-dotnet-roslyn-analyzers-755ceda7/nuget/v3/index.json", "username":"optional", "password":"'$internalPackageFeedPat'"},
-    {"endpoint":"https://pkgs.dev.azure.com/dnceng/_packaging/darc-int-dotnet-runtime-556582d9-3/nuget/v3/index.json", "username":"optional", "password":"'$internalPackageFeedPat'"},
-    {"endpoint":"https://pkgs.dev.azure.com/dnceng/_packaging/darc-int-dotnet-runtime-556582d9-2/nuget/v3/index.json", "username":"optional", "password":"'$internalPackageFeedPat'"},
-    {"endpoint":"https://pkgs.dev.azure.com/dnceng/public/_packaging/darc-pub-dotnet-templating-3cdb1866-1/nuget/v3/index.json", "username":"optional", "password":"'$internalPackageFeedPat'"},
-    {"endpoint":"https://pkgs.dev.azure.com/dnceng/public/_packaging/darc-pub-dotnet-templating-3cdb1866/nuget/v3/index.json", "username":"optional", "password":"'$internalPackageFeedPat'"},
-    {"endpoint":"https://pkgs.dev.azure.com/dnceng/_packaging/darc-int-dotnet-aspnetcore-67acc3d3/nuget/v3/index.json", "username":"optional", "password":"'$internalPackageFeedPat'"},
-    {"endpoint":"https://pkgs.dev.azure.com/dnceng/_packaging/darc-int-dotnet-aspnetcore-67acc3d3-1/nuget/v3/index.json", "username":"optional", "password":"'$internalPackageFeedPat'"},
-    {"endpoint":"https://pkgs.dev.azure.com/dnceng/_packaging/darc-int-dotnet-aspnetcore-67acc3d3-2/nuget/v3/index.json", "username":"optional", "password":"'$internalPackageFeedPat'"},
-    {"endpoint":"https://pkgs.dev.azure.com/dnceng/internal/_packaging/dotnet5-internal-transport/nuget/v3/index.json", "username":"optional", "password":"'$internalPackageFeedPat'"},
-    {"endpoint":"https://pkgs.dev.azure.com/dnceng/_packaging/5.0.204-servicing.21263.62-shipping/nuget/v3/index.json", "username":"optional", "password":"'$internalPackageFeedPat'"}
-  ]}'
-fi
-
-set -x
-scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
-
-# runtime 3.1.1 required for darc
-"$scriptroot/eng/common/dotnet-install.sh"  -runtime dotnet -version 3.1.1
+export NUGET_PACKAGES=$restoredPackagesDir/
 
 if [ "$alternateTarget" == "true" ]; then
-  "$CLIPATH/dotnet" $SDKPATH/MSBuild.dll "$scriptroot/build.proj" /bl:source-build-test.binlog /flp:v=diag /clp:v=m "$@"
+  "$CLI_ROOT/dotnet" $CLI_ROOT/sdk/$SDK_VERSION/MSBuild.dll "$SCRIPT_ROOT/build.proj" /bl:source-build-test.binlog /clp:v=m ${MSBUILD_ARGUMENTS[@]} "$@"
 else
-  "$scriptroot/eng/common/build.sh" --restore --build -c Release --warnaserror false /bl:$scriptroot/artifacts/log/Debug/Build_$(date +"%m%d%H%M%S").binlog /flp:v=diag "$@"
+  LogDateStamp=$(date +"%m%d%H%M%S")
+  $CLI_ROOT/dotnet $CLI_ROOT/sdk/$SDK_VERSION/MSBuild.dll /bl:$SCRIPT_ROOT/artifacts/log/Debug/BuildXPlatTasks_$LogDateStamp.binlog $SCRIPT_ROOT/tools-local/init-build.proj /t:PrepareOfflineLocalTools ${MSBUILD_ARGUMENTS[@]} "$@"
+
+  $CLI_ROOT/dotnet $CLI_ROOT/sdk/$SDK_VERSION/MSBuild.dll /bl:$SCRIPT_ROOT/artifacts/log/Debug/Build_$LogDateStamp.binlog $SCRIPT_ROOT/build.proj ${MSBUILD_ARGUMENTS[@]} "$@"
 fi
